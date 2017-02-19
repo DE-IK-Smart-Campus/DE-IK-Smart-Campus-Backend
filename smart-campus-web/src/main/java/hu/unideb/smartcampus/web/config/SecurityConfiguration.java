78e1a94252b13b6256a20c9795550da9d66508fb
Kovács-Ferenc Norbert <kfnorbi@gmail.com>
2017-02-19 15:44:43 +0100

    Spring security configuration

---
+++ smart-campus-web/src/main/java/hu/unideb/smartcampus/web/config/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package hu.unideb.smartcampus.web.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@SuppressWarnings({"PMD.SignatureDeclareThrowsException","checkstyle:indentation"})
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http.authorizeRequests()
+                .antMatchers("/", "/home")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated()    
+             .and()
+                 .httpBasic()
+             .and()
+                 .rememberMe();
+        // @formatter:on
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    }
+
+}
