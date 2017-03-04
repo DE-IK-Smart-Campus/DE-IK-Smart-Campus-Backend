188f32af0155bc812f07b42af57ae867cd547964
atka66 <atkavagyok@gmail.com>
2017-03-04 04:04:38 +0100

    pwithwildfly in install.sh

--- config/script/install.sh
+++ config/script/install.sh
@@ -5,5 +5,6 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
 	if [ "$TRAVIS_BRANCH" = "test" ]; then
 		mvn clean install -Ptest -Dwildfly.hostname=$TEST_HOSTNAME -Dwildfly.username=$WILDFLY_TEST_USER -Dwildfly.password=$WILDFLY_TEST_PASSWORD -Dmysql.connection.url=$TEST_MYSQL_URL -Dmysql.username=$TEST_MYSQL_USER -Dmysql.password=$TEST_MYSQL_PASSWORD
-	fi
+	else
+		mvn clean install -Pwith-wildfly
 fi
