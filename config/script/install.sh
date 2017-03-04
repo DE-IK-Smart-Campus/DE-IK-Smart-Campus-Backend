c4d308c3b9698fb9c82c51e03aaa31e5d1c9b50e
atka66 <atkavagyok@gmail.com>
2017-03-04 04:06:12 +0100

    fixing missing fi

--- config/script/install.sh
+++ config/script/install.sh
@@ -7,4 +7,5 @@ else
 		mvn clean install -Ptest -Dwildfly.hostname=$TEST_HOSTNAME -Dwildfly.username=$WILDFLY_TEST_USER -Dwildfly.password=$WILDFLY_TEST_PASSWORD -Dmysql.connection.url=$TEST_MYSQL_URL -Dmysql.username=$TEST_MYSQL_USER -Dmysql.password=$TEST_MYSQL_PASSWORD
 	else
 		mvn clean install -Pwith-wildfly
+	fi
 fi
