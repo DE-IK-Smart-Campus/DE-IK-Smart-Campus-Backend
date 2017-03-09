31528050e1a0f54ea1c2c20930018cd7f0ecc193
Kovács-Ferenc Norbert <kfnorbi@gmail.com>
2017-03-10 00:36:47 +0100

    Ldap configuration

--- smart-campus-web/src/main/java/hu/unideb/smartcampus/web/config/WebConfiguration.java
+++ smart-campus-web/src/main/java/hu/unideb/smartcampus/web/config/WebConfiguration.java
@@ -1,8 +1,11 @@
 package hu.unideb.smartcampus.web.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import hu.unideb.smartcampus.service.api.config.ServiceConfiguration;
 import hu.unideb.smartcampus.webservice.api.config.WebServiceConfiguration;
@@ -13,5 +16,18 @@ import hu.unideb.smartcampus.webservice.api.config.WebServiceConfiguration;
 @Configuration
 @Import({ServiceConfiguration.class, WebServiceConfiguration.class})
 @ComponentScan("hu.unideb.smartcampus.web.controller")
+@PropertySource("classpath:ldap.properties")
 public class WebConfiguration {
+
+
+  /**
+   * Construct an instance of PropertySourcesPlaceholderConfigurer.
+   *
+   * @return a bean instance of PropertySourcesPlaceholderConfigurer.
+   */
+  @Bean
+  public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+    return new PropertySourcesPlaceholderConfigurer();
+  }
+
 }
