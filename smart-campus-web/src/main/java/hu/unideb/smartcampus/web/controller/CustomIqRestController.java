fbbc4e99031ae4326e05b3cef0418f61a54a3f63
Nándor Holozsnyák <holi60@gmail.com>
2017-03-20 01:12:41 +0100

    Ask subjects as new IQ

--- smart-campus-web/src/main/java/hu/unideb/smartcampus/web/controller/CustomIqRestController.java
+++ smart-campus-web/src/main/java/hu/unideb/smartcampus/web/controller/CustomIqRestController.java
@@ -1,7 +1,8 @@
 package hu.unideb.smartcampus.web.controller;
 
 
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.List;
 
 import org.jivesoftware.smack.AbstractXMPPConnection;
 import org.jivesoftware.smack.SmackException.NotConnectedException;
@@ -11,10 +12,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import hu.unideb.smartcampus.service.api.xmpp.DefaultUser;
 import hu.unideb.smartcampus.service.api.xmpp.EjabberdUser;
+import hu.unideb.smartcampus.shared.iq.SubjectsIq;
 import hu.unideb.smartcampus.shared.iq.TestIq;
+import hu.unideb.smartcampus.shared.iq.TestIq.Thing;
 
 
 /**
@@ -34,11 +39,17 @@ public class CustomIqRestController {
   private final EjabberdUser ejabberdUser;
 
   /**
+   * Default user.
+   */
+  private final DefaultUser defaultUser;
+
+  /**
    * Constructor for dependency injection.
    */
   @Autowired
-  public CustomIqRestController(final EjabberdUser sampleService) {
+  public CustomIqRestController(final EjabberdUser sampleService, final DefaultUser defaultUser) {
     this.ejabberdUser = sampleService;
+    this.defaultUser = defaultUser;
   }
 
   /**
@@ -51,7 +62,9 @@ public class CustomIqRestController {
     ResponseEntity<String> body = ResponseEntity.ok().body("OK");
     try {
       AbstractXMPPConnection connection = ejabberdUser.getConnection();
-      TestIq packet = new TestIq("smartcampus@smartcampus", Collections.emptyList());
+      List<Thing> things =
+          Arrays.asList(new Thing("Uj IQ provider", "Smart campus appra a legjobb"));
+      TestIq packet = new TestIq("smartcampus@smartcampus", things);
       packet.setTo("smartcampus@smartcampus/Smartcampus");
       packet.setType(Type.set);
       packet.setFrom(connection.getUser());
@@ -60,6 +73,41 @@ public class CustomIqRestController {
       LOGGER.error("Error while sending IQ", e);
       ResponseEntity.badRequest().body(e.getCause().getMessage());
     }
+    return body;
+  }
+  
+  /**
+   * Sample endpoint.
+   * 
+   * @return {@link ResponseEntity}
+   */
+  @GetMapping(path = "/subjects")
+  public ResponseEntity<String> sendSubjectRetrieval(@RequestParam(name = "user") String user) {
+    ResponseEntity<String> body = ResponseEntity.ok().body("OK");
+    try {
+      AbstractXMPPConnection connection = ejabberdUser.getConnection();
+      SubjectsIq iq = new SubjectsIq();
+      iq.setType(Type.get);
+      iq.setFrom(connection.getUser());
+      iq.setTo(connection.getUser());
+      iq.setStudent(user);
+      connection.sendStanza(iq);
+    } catch (NotConnectedException e) {
+      LOGGER.error("Error while sending IQ", e);
+      ResponseEntity.badRequest().body(e.getCause().getMessage());
+    }
+    return body;
+  }
+
+  /**
+   * Sample endpoint.
+   * 
+   * @return {@link ResponseEntity}
+   */
+  @GetMapping(path = "/smartcampusRe")
+  public ResponseEntity<String> reconnectSmartcampusUser() {
+    ResponseEntity<String> body = ResponseEntity.ok().body("OK");
+    defaultUser.reconnect();
     return body;
   }
 }
