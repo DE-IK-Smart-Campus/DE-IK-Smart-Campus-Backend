c42ef00db22cd0bc18c5de0ee481b70e2bd37070
atka66 <atkavagyok@gmail.com>
2017-03-22 15:57:17 +0100

    OtherController tests

---
+++ smart-campus-web/src/test/java/hu/unideb/smartcampus/web/controller/OtherControllerTest.java
@@ -0,0 +1,35 @@
+package hu.unideb.smartcampus.web.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import hu.unideb.smartcampus.web.controller.dashboard.OtherController;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OtherControllerTest extends AbstractControllerTest{
+  private static final String REQUEST_URL_DASHBOARD_OTHER = "/dashboard/other";
+  private static final String VIEW_NAME_DASHBOARD_OTHER = "dashboard/other";
+
+  @InjectMocks
+  private OtherController otherController;
+
+  @Override
+  protected Object[] controllerUnderTest() {
+    return new Object[]{this.otherController};
+  }
+
+  @Test
+  public void loadOtherViewShouldLoadOtherView() throws Exception {
+    this.mockMvc.perform(get(REQUEST_URL_DASHBOARD_OTHER))
+        .andExpect(status().isOk())
+        .andExpect(view().name(VIEW_NAME_DASHBOARD_OTHER))
+        .andExpect(forwardedUrl(VIEW_PREFIX + VIEW_NAME_DASHBOARD_OTHER + VIEW_SUFFIX));
+  }
+}
