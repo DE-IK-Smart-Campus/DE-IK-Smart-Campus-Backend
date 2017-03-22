de714754af15b8f7581d89cbbefb26105fe8604e
atka66 <atkavagyok@gmail.com>
2017-03-22 15:51:06 +0100

    CalendarController tests

---
+++ smart-campus-web/src/test/java/hu/unideb/smartcampus/web/controller/CalendarControllerTest.java
@@ -0,0 +1,36 @@
+package hu.unideb.smartcampus.web.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import hu.unideb.smartcampus.web.controller.dashboard.CalendarController;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CalendarControllerTest extends AbstractControllerTest {
+
+  private static final String REQUEST_URL_DASHBOARD_CALENDAR = "/dashboard/calendar";
+  private static final String VIEW_NAME_DASHBOARD_CALENDAR = "dashboard/calendar";
+
+  @InjectMocks
+  private CalendarController calendarController;
+
+  @Override
+  protected Object[] controllerUnderTest() {
+    return new Object[]{this.calendarController};
+  }
+
+  @Test
+  public void loadCalendarViewShouldLoadCalendarView() throws Exception {
+    this.mockMvc.perform(get(REQUEST_URL_DASHBOARD_CALENDAR))
+        .andExpect(status().isOk())
+        .andExpect(view().name(VIEW_NAME_DASHBOARD_CALENDAR))
+        .andExpect(forwardedUrl(VIEW_PREFIX + VIEW_NAME_DASHBOARD_CALENDAR + VIEW_SUFFIX));
+  }
+}
