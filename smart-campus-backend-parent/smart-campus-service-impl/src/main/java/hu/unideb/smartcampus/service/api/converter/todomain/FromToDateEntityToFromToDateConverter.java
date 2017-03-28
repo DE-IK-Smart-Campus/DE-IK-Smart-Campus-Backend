c5b95be8786ba8c0dad49d84bedc91f5ad494c84
Iványi-Nagy Gábor <gabilampard@gmail.com>
2017-03-28 23:25:11 +0200

    Updated converters

--- smart-campus-backend-parent/smart-campus-service-impl/src/main/java/hu/unideb/smartcampus/service/api/converter/todomain/FromToDateEntityToFromToDateConverter.java
+++ smart-campus-backend-parent/smart-campus-service-impl/src/main/java/hu/unideb/smartcampus/service/api/converter/todomain/FromToDateEntityToFromToDateConverter.java
@@ -1,7 +1,20 @@
 package hu.unideb.smartcampus.service.api.converter.todomain;
 
-/**
- * Created by gabichelsea on 3/28/17.
- */
-public class FromToDateEntityToFromToDateConverter {
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import hu.unideb.smartcampus.persistence.entity.FromToDateEmbeddedEntity;
+import hu.unideb.smartcampus.service.api.domain.FromToDate;
+
+@Component
+public class FromToDateEntityToFromToDateConverter implements Converter<FromToDateEmbeddedEntity, FromToDate> {
+
+  @Override
+  public FromToDate convert(final FromToDateEmbeddedEntity fromToDateEmbeddedEntity) {
+    return fromToDateEmbeddedEntity == null ? null
+        : FromToDate.builder()
+            .fromDate(fromToDateEmbeddedEntity.getFromDate())
+            .toDate(fromToDateEmbeddedEntity.getToDate())
+            .build();
+  }
 }
