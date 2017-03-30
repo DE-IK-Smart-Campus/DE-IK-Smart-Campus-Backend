54f44dc2f2578ae1fb94d35ce051b6e2a1eefd22
Fekete Attila <atkavagyok@gmail.com>
2017-03-30 18:35:40 +0200

    no debug + no auto subscribe + no otr

--- smart-campus-client-parent/smart-campus-client-web/src/main/resources/static/js/converse-initialize.js
+++ smart-campus-client-parent/smart-campus-client-web/src/main/resources/static/js/converse-initialize.js
@@ -6,21 +6,20 @@ if (location.hostname === "smartcampus"
 }
 converse.initialize(
     {
-        bosh_service_url: location.protocol + "//" + location.hostname + port + '/http-bind/',
+        bosh_service_url: location.protocol + "//" + location.hostname + port + '/http-bind',
         credentials_url: location.origin + '/smartcampus-client/xmppcredentials',
         muc_domain:'conference.smartcampus',
         show_controlbox_by_default: true,
         authentication: 'login',
-        debug: true,
         keepalive: true,
         allow_logout: false,
         allow_registration: false,
+        allow_otr: false,
         auto_login: true,
         i18n: 'hu',
         roster_groups:true,
         muc_nickname_from_jid:true,
         hide_muc_server:true,
-        auto_subscribe:true,
         auto_join_on_invite:true,
         visible_toolbar_buttons: {
             call: false,
