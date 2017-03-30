cdec7617cc39cb8dbc6dbfe80f12f7b5cd2d34cf
Fekete Attila <atkavagyok@gmail.com>
2017-03-30 23:35:02 +0200

    removed mock from ConsultingHoursController, removed webkit stuff from main.css

--- smart-campus-client-parent/smart-campus-client-web/src/main/java/hu/unideb/smartcampus/web/controller/dashboard/ConsultingHoursController.java
+++ smart-campus-client-parent/smart-campus-client-web/src/main/java/hu/unideb/smartcampus/web/controller/dashboard/ConsultingHoursController.java
@@ -15,12 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
-import java.util.Arrays;
 import hu.unideb.smartcampus.service.api.xmpp.EjabberdUser;
 import hu.unideb.smartcampus.shared.iq.request.SubjectsIqRequest;
-import hu.unideb.smartcampus.shared.wrapper.SubjectRetrievalResponseWrapper;
-import hu.unideb.smartcampus.shared.wrapper.inner.InstructorWrapper;
-import hu.unideb.smartcampus.shared.wrapper.inner.SubjectWrapper;
 
 /**
  * TODO.
@@ -59,7 +55,6 @@ public class ConsultingHoursController {
 
   /**
    * TODO.
-   * 
    * @return model and view.
    */
   @GetMapping
@@ -85,43 +80,5 @@ public class ConsultingHoursController {
 
   private String getUser(AbstractXMPPConnection connection) {
     return connection.getUser().toString().split("@")[0];
-  }
-
-  /**
-   * TODO.
-   * 
-   * @return asd
-   */
-  public SubjectRetrievalResponseWrapper mockSubjectRetrievalResponseWrapper() {
-    return new SubjectRetrievalResponseWrapper(
-        "AskSubjectsProcessMessageResponse",
-        Arrays.asList(
-            new SubjectWrapper(
-                "Hálózati architektúrák és protokollok",
-                Arrays.asList(
-                    new InstructorWrapper(
-                        1L,
-                        "Dr. Gál Zoltán"),
-                    new InstructorWrapper(
-                        2L,
-                        "Dr. Szilágyi Szabolcs"),
-                    new InstructorWrapper(
-                        3L,
-                        "Vas Ádám"))),
-            new SubjectWrapper(
-                "Az internet eszközei és szolgáltatásai",
-                Arrays.asList(
-                    new InstructorWrapper(
-                        4L,
-                        "Dr. Jeszenszky Péter"))),
-            new SubjectWrapper(
-                "A mesterséges intelligencia alapjai",
-                Arrays.asList(
-                    new InstructorWrapper(
-                        5L,
-                        "Dr. Várterész Magdolna"),
-                    new InstructorWrapper(
-                        6L,
-                        "Dr. Horváth Géza")))));
   }
 }
