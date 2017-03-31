f1d2edca5d84a4c8e0ec141a41a42715f839617a
Fekete Attila <atkavagyok@gmail.com>
2017-04-01 00:43:28 +0200

    working instructor view

--- smart-campus-client-parent/smart-campus-client-web/src/main/java/hu/unideb/smartcampus/web/controller/dashboard/ConsultingHoursController.java
+++ smart-campus-client-parent/smart-campus-client-web/src/main/java/hu/unideb/smartcampus/web/controller/dashboard/ConsultingHoursController.java
@@ -1,17 +1,28 @@
 package hu.unideb.smartcampus.web.controller.dashboard;
 
+import org.jivesoftware.smack.AbstractXMPPConnection;
+import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.StanzaCollector;
+import org.jivesoftware.smack.packet.IQ;
+import org.jxmpp.jid.impl.JidCreate;
+import org.jxmpp.stringprep.XmppStringprepException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
+import java.util.List;
 import hu.unideb.smartcampus.service.api.ConsultingHoursService;
+import hu.unideb.smartcampus.service.api.xmpp.EjabberdUser;
+import hu.unideb.smartcampus.shared.iq.request.InstructorConsultingDatesIqRequest;
 import hu.unideb.smartcampus.shared.iq.request.SubjectsIqRequest;
+import hu.unideb.smartcampus.shared.iq.request.element.ConsultingDateIqElement;
 
 /**
  * TODO.
@@ -25,6 +36,9 @@ public class ConsultingHoursController {
   @Autowired
   private ConsultingHoursService consultingHoursService;
 
+  @Autowired
+  private EjabberdUser ejabberdUser;
+
   /**
    * TODO.
    */
@@ -37,11 +51,19 @@ public class ConsultingHoursController {
   /**
    * TODO.
    */
-  private static final String SUBJECT_RETRIEVAL_RESPONSE_MODEL_OBJECT_NAME = "subjectRetrievalResponse";
+  private static final String CURRENT_USERNAME_MODEL_OBJECT_NAME = "currentUsername";
   /**
    * TODO.
    */
-  private static final String CURRENT_USERNAME_MODEL_OBJECT_NAME = "currentUsername";
+  private static final String SUBJECT_LIST_MODEL_OBJECT_NAME = "subjectList";
+  /**
+   * TODO.
+   */
+  private static final String INSTRUCTOR_NAME_MODEL_OBJECT_NAME = "instructorName";
+  /**
+   * TODO.
+   */
+  private static final String CONSULTING_DATE_LIST_MODEL_OBJECT_NAME = "consultingDateList";
 
   /**
    * TODO.
@@ -54,23 +76,42 @@ public class ConsultingHoursController {
     modelAndView.addObject(CURRENT_USERNAME_MODEL_OBJECT_NAME, name);
 
     final SubjectsIqRequest subjects = consultingHoursService.getSubjects();
-    modelAndView.addObject(SUBJECT_RETRIEVAL_RESPONSE_MODEL_OBJECT_NAME, subjects);
+    modelAndView.addObject(SUBJECT_LIST_MODEL_OBJECT_NAME, subjects);
 
     return modelAndView;
   }
+
 
   /**
    * TODO.
    * @return model and view.
    */
-  @GetMapping
-  public ModelAndView loadInstructorViewByInstructorId(final Principal principal, @RequestParam(value = "id") Long instructorId) {
+  @GetMapping("/instructor/{instructorId}")
+  public ModelAndView loadInstructorViewByInstructorId(final Principal principal, @PathVariable final Long instructorId) {
     final ModelAndView modelAndView = new ModelAndView(CONSULTING_HOURS_INSTRUCTOR_VIEW);
     final String name = principal.getName();
     modelAndView.addObject(CURRENT_USERNAME_MODEL_OBJECT_NAME, name);
 
-    final SubjectsIqRequest subjects = consultingHoursService.getSubjects();
-    modelAndView.addObject(SUBJECT_RETRIEVAL_RESPONSE_MODEL_OBJECT_NAME, subjects);
+    String instructorName = "";
+    List<ConsultingDateIqElement> consultingDateList = null;
+    try {
+      AbstractXMPPConnection connection = ejabberdUser.getConnection();
+      InstructorConsultingDatesIqRequest iq = new InstructorConsultingDatesIqRequest();
+      iq.setType(IQ.Type.get);
+      iq.setFrom(connection.getUser());
+      iq.setTo(JidCreate.from("smartcampus@smartcampus/Smartcampus"));
+      iq.setInstructorId(instructorId.toString());
+      StanzaCollector collector = connection.createStanzaCollectorAndSend(iq);
+      InstructorConsultingDatesIqRequest resultIq = collector.nextResultBlockForever();
+      instructorName = resultIq.getInstructorName();
+      consultingDateList = resultIq.getConsultingDates();
+    } catch (SmackException.NotConnectedException | InterruptedException | XmppStringprepException e) {
+      LOGGER.error("Error while sending IQ", e);
+      ResponseEntity.badRequest().body(e.getCause().getMessage());
+    }
+
+    modelAndView.addObject(INSTRUCTOR_NAME_MODEL_OBJECT_NAME, instructorName);
+    modelAndView.addObject(CONSULTING_DATE_LIST_MODEL_OBJECT_NAME, consultingDateList);
 
     return modelAndView;
   }
