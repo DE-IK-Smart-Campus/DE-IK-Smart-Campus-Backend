ab4f649698239903e71080234cf81b921cbb4623
Fekete Attila <atkavagyok@gmail.com>
2017-04-03 00:53:20 +0200

    removing other and about-us stuff (pretty useless), adding config for i18n, adding resource bundles, using them in views

--- smart-campus-client-parent/smart-campus-client-web/src/main/java/hu/unideb/smartcampus/web/config/WebConfiguration.java
+++ smart-campus-client-parent/smart-campus-client-web/src/main/java/hu/unideb/smartcampus/web/config/WebConfiguration.java
@@ -5,7 +5,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.Locale;
 import hu.unideb.smartcampus.service.api.ServiceConfiguration;
 
 
@@ -15,7 +22,7 @@ import hu.unideb.smartcampus.service.api.ServiceConfiguration;
 @Configuration
 @Import({ServiceConfiguration.class})
 @ComponentScan("hu.unideb.smartcampus.web")
-public class WebConfiguration {
+public class WebConfiguration extends WebMvcConfigurerAdapter {
 
 
   /**
@@ -28,4 +35,29 @@ public class WebConfiguration {
     return new PropertySourcesPlaceholderConfigurer();
   }
 
+  @Bean
+  public ResourceBundleMessageSource messageSource() {
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    messageSource.setBasenames("i18n/index", "i18n/dashboard", "i18n/home", "i18n/calendar", "i18n/consulting_hours");
+    return messageSource;
+  }
+
+  @Bean
+  public LocaleResolver localeResolver() {
+    SessionLocaleResolver slr = new SessionLocaleResolver();
+    slr.setDefaultLocale(Locale.ENGLISH);
+    return slr;
+  }
+
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+    lci.setParamName("lang");
+    return lci;
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(localeChangeInterceptor());
+  }
 }
