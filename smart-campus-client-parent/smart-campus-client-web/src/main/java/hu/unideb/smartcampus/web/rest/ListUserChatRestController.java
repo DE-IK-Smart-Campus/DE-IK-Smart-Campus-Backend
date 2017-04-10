ad2d48240c1ae47d88c17d8414baae2399f08d05
Nándor Holozsnyák <holi60@gmail.com>
2017-04-10 13:19:00 +0200

    Listing user single and muc rooms IQ with provider

--- smart-campus-client-parent/smart-campus-client-web/src/main/java/hu/unideb/smartcampus/web/rest/ListUserChatRestController.java
+++ smart-campus-client-parent/smart-campus-client-web/src/main/java/hu/unideb/smartcampus/web/rest/ListUserChatRestController.java
@@ -1,5 +1,4 @@
-package hu.unideb.smartcampus.web.controller;
-
+package hu.unideb.smartcampus.web.rest;
 
 import java.util.List;
 
@@ -18,34 +17,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import hu.unideb.smartcampus.service.api.xmpp.DefaultUser;
 import hu.unideb.smartcampus.service.api.xmpp.EjabberdUser;
 import hu.unideb.smartcampus.shared.iq.request.AddMucChatIqRequest;
 import hu.unideb.smartcampus.shared.iq.request.AddUserChatIqRequest;
-import hu.unideb.smartcampus.shared.iq.request.InstructorConsultingDatesIqRequest;
 import hu.unideb.smartcampus.shared.iq.request.ListUserChatsIqRequest;
-import hu.unideb.smartcampus.shared.iq.request.SignUpForConsultingDateIqRequest;
-import hu.unideb.smartcampus.shared.iq.request.SubjectsIqRequest;
-import hu.unideb.smartcampus.shared.iq.request.element.ConsultingDateIqElement;
-import hu.unideb.smartcampus.shared.iq.request.element.SubjectIqElement;
 
-
-/**
- * Sample rest controller.
- */
 @RestController
-public class CustomIqRestController {
+public class ListUserChatRestController {
 
-  /**
-   * Smartcampus user JID.
-   */
-  private static final String SMARTCAMPUS_SMARTCAMPUS_SMARTCAMPUS =
-      "smartcampus@smartcampus/Smartcampus";
-
-  /**
-   * Logger.
-   */
-  private static final Logger LOGGER = LoggerFactory.getLogger(CustomIqRestController.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(ListUserChatRestController.class);
 
   /**
    * Sample dependency.
@@ -55,96 +35,20 @@ public class CustomIqRestController {
   /**
    * Default user.
    */
-  private final DefaultUser defaultUser;
 
   /**
    * Constructor for dependency injection.
    */
   @Autowired
-  public CustomIqRestController(final EjabberdUser sampleService, final DefaultUser defaultUser) {
+  public ListUserChatRestController(final EjabberdUser sampleService) {
     this.ejabberdUser = sampleService;
-    this.defaultUser = defaultUser;
   }
 
   /**
-   * Sample endpoint.
-   * 
-   * @return {@link ResponseEntity}
+   * Smartcampus user JID.
    */
-  @GetMapping(path = "/subjects")
-  public ResponseEntity<List<SubjectIqElement>> sendSubjectRetrieval(
-      @RequestParam(name = "user") String user) {
-    SubjectsIqRequest resultIq = null;
-    try {
-      AbstractXMPPConnection connection = ejabberdUser.getConnection();
-      SubjectsIqRequest iq = new SubjectsIqRequest();
-      iq.setType(Type.get);
-      iq.setFrom(connection.getUser());
-      iq.setTo(JidCreate.from(SMARTCAMPUS_SMARTCAMPUS_SMARTCAMPUS));
-      iq.setStudent(user);
-      StanzaCollector collector = connection.createStanzaCollectorAndSend(iq);
-      resultIq = collector.nextResultBlockForever();
-    } catch (NotConnectedException | InterruptedException | XmppStringprepException e) {
-      LOGGER.error("Error while sending IQ", e);
-      ResponseEntity.badRequest().body(e.getCause().getMessage());
-    }
-    return ResponseEntity.ok().body(resultIq.getSubjects());
-  }
-
-  /**
-   * Sample endpoint.
-   * 
-   * @return {@link ResponseEntity}
-   */
-  @GetMapping(path = "/consulting")
-  public ResponseEntity<List<ConsultingDateIqElement>> getInstructorConsultingDate(
-      @RequestParam(name = "id") String id) {
-    List<ConsultingDateIqElement> result = null;
-    try {
-      AbstractXMPPConnection connection = ejabberdUser.getConnection();
-      InstructorConsultingDatesIqRequest iq = new InstructorConsultingDatesIqRequest();
-      iq.setType(Type.get);
-      iq.setFrom(connection.getUser());
-      iq.setTo(JidCreate.from(SMARTCAMPUS_SMARTCAMPUS_SMARTCAMPUS));
-      iq.setInstructorId(id);
-      StanzaCollector collector = connection.createStanzaCollectorAndSend(iq);
-      InstructorConsultingDatesIqRequest resultIq = collector.nextResultBlockForever();
-      result = resultIq.getConsultingDates();
-    } catch (NotConnectedException | InterruptedException | XmppStringprepException e) {
-      LOGGER.error("Error while sending IQ", e);
-      ResponseEntity.badRequest().body(e.getCause().getMessage());
-    }
-    return ResponseEntity.ok().body(result);
-  }
-
-  /**
-   * Sample endpoint.
-   * 
-   * @return {@link ResponseEntity}
-   */
-  @GetMapping(path = "/signUp")
-  public ResponseEntity<String> signUpForConsultingDate(@RequestParam(name = "id") String id,
-      @RequestParam(name = "userId") String userId) {
-    SignUpForConsultingDateIqRequest result = null;
-    try {
-      AbstractXMPPConnection connection = ejabberdUser.getConnection();
-      SignUpForConsultingDateIqRequest iq = new SignUpForConsultingDateIqRequest();
-      iq.setType(Type.set);
-      iq.setFrom(connection.getUser());
-      iq.setTo(JidCreate.from(SMARTCAMPUS_SMARTCAMPUS_SMARTCAMPUS));
-      iq.setConsultingHourId(Long.valueOf(id));
-      iq.setDuration("10 minutes");
-      iq.setUserId(userId);
-      iq.setReason("Cause I wanna talk man.");
-      StanzaCollector collector = connection.createStanzaCollectorAndSend(iq);
-      SignUpForConsultingDateIqRequest resultIq = collector.nextResultBlockForever();
-      result = resultIq;
-    } catch (NotConnectedException | InterruptedException | XmppStringprepException e) {
-      LOGGER.error("Error while sending IQ", e);
-      ResponseEntity.badRequest().body(e.getCause().getMessage());
-    }
-    return ResponseEntity.ok().body(result.getResponseMessage());
-  }
+  private static final String SMARTCAMPUS_SMARTCAMPUS_SMARTCAMPUS =
+      "smartcampus@smartcampus/Smartcampus";
 
   @GetMapping(path = "/add/chat/{student}")
   public ResponseEntity<String> addChatRoom(@PathVariable("student") String student,
@@ -191,7 +95,7 @@ public class CustomIqRestController {
     ListUserChatsIqRequest iq = new ListUserChatsIqRequest();
     try {
       AbstractXMPPConnection connection = ejabberdUser.getConnection();
-      iq.setType(Type.set);
+      iq.setType(Type.get);
       iq.setFrom(connection.getUser());
       iq.setTo(JidCreate.from(SMARTCAMPUS_SMARTCAMPUS_SMARTCAMPUS));
       iq.setStudent(student);
@@ -209,7 +113,7 @@ public class CustomIqRestController {
     ListUserChatsIqRequest iq = new ListUserChatsIqRequest();
     try {
       AbstractXMPPConnection connection = ejabberdUser.getConnection();
-      iq.setType(Type.set);
+      iq.setType(Type.get);
       iq.setFrom(connection.getUser());
       iq.setTo(JidCreate.from(SMARTCAMPUS_SMARTCAMPUS_SMARTCAMPUS));
       iq.setStudent(student);
@@ -220,18 +124,6 @@ public class CustomIqRestController {
       ResponseEntity.badRequest().body(e.getCause().getMessage());
     }
     return ResponseEntity.ok().body(iq.getChatList());
-  }
-
-  /**
-   * Sample endpoint.
-   * 
-   * @return {@link ResponseEntity}
-   */
-  @GetMapping(path = "/smartcampusRe")
-  public ResponseEntity<String> reconnectSmartcampusUser() {
-    ResponseEntity<String> body = ResponseEntity.ok().body("OK");
-    defaultUser.reconnect();
-    return body;
   }
 
 }
--- smart-campus-shared-parent/smart-campus-shared-iq/src/test/java/hu/unideb/smartcampus/shared/iq/request/UserChatListIqParserTest.java
+++ smart-campus-shared-parent/smart-campus-shared-iq/src/test/java/hu/unideb/smartcampus/shared/iq/request/UserChatListIqParserTest.java
@@ -16,19 +16,25 @@ public class UserChatListIqParserTest extends AbstractParserTest {
 
   private static final String STUDENT = "Emily Stock";
   private static final List<String> CHAT_LIST = Arrays.asList("AndroidMember", "WebMember");
+  private static final List<String> MUC_CHAT_LIST = Arrays.asList("Room1", "Room2");
 
   @Test
   public void testIqProvider() throws Exception {
-    ListUserChatIqRequest iq =
-        ListUserChatIqRequest.builder().student(STUDENT).chatList(CHAT_LIST).build();
-    ListUserChatIqRequest parse = getParsedObject(iq);
+    ListUserChatsIqRequest iq =
+        ListUserChatsIqRequest.builder()
+        .student(STUDENT)
+        .chatList(CHAT_LIST)
+        .mucChatList(MUC_CHAT_LIST)
+        .build();
+    ListUserChatsIqRequest parse = getParsedObject(iq);
     Assert.assertEquals(STUDENT, parse.getStudent());
     Assert.assertEquals(CHAT_LIST, parse.getChatList());
+    Assert.assertEquals(MUC_CHAT_LIST,parse.getMucChatList());
   }
 
   @Override
   public String getElement() {
-    return ListUserChatIqRequest.ELEMENT;
+    return ListUserChatsIqRequest.ELEMENT;
   }
 
   @Override
