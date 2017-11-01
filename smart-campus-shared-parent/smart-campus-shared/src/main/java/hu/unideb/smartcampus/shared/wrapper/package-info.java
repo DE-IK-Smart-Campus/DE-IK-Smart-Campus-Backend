6a9e13374358402df26ae6133f00dddf480065e5
Bak Balázs <bak.balazs95@gmail.com>
2017-11-01 17:34:36 +0100

    test

--- smart-campus-shared-parent/smart-campus-shared/src/main/java/hu/unideb/smartcampus/shared/wrapper/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * These wrapper classes will be responsible to provide response to users over XMPP protocoll, these
- * wrappers would transfer the data/messages, so they HAVE TO set the BaseWrapper's messageType.
- */
-package hu.unideb.smartcampus.shared.wrapper;
