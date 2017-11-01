acd9682437722bda2105794f16508d1121b5d35f
Bak Balázs <bak.balazs95@gmail.com>
2017-11-01 16:04:50 +0100

    shared delete

--- smart-campus-shared-parent/smart-campus-shared/src/main/java/hu/unideb/smartcampus/shared/wrapper/inner/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * These wrapper classes are members of another wrapper classes, so they do not have to set the
- * BaseWrapper's class messageType.
- */
-package hu.unideb.smartcampus.shared.wrapper.inner;
